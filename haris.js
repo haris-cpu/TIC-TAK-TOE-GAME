de802ff3ff367b5be1959e16f37efddee991cff8
M Haris Jabbar <harisjabbar24@gmail.com>
2024-07-26 00:13:47 +0500

    Add files via upload

---
+++ haris.js
@@ -0,0 +1,70 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn=document.querySelector("#newGame-btn")
+let msgContainer=document.querySelector(".msg-container")
+let msg=document.querySelector("#msg");
+let turnO = true; // true for player O, false for player X
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+const resetGame=()=>{
+    let turnO = true;
+    enabledBoxes();
+    msgContainer.classList.add("hide")
+
+}
+
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (turnO) {
+            box.innerText = "O";
+            turnO = false; // correctly set turnO to boolean false
+        } else {
+            box.innerText = "X";
+            turnO = true; // correctly set turnO to boolean true
+        }
+        box.disabled = true;
+        checkWinner();
+    });
+});
+
+const disabledBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+}
+const enabledBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+}
+
+const showWinner=(winner)=>{
+    msg.innerText=`Congratulations,Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disabledBoxes();
+}
+const checkWinner=()=>{
+    for(let pattern of winPatterns){
+            let pos1Val= boxes[pattern[0]].innerText;
+            let pos2Val= boxes[pattern[1]].innerText;
+            let pos3Val= boxes[pattern[2]].innerText;
+    if(pos1Val!=""&& pos2Val!=""&&pos3Val!=""){
+        if(pos1Val==pos2Val&&pos2Val==pos3Val){
+            showWinner(pos1Val);
+        }
+    }
+    }
+};
+newGameBtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click",resetGame);
